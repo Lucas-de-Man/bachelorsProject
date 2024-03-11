09327f9002a7c1e9a634fb3b7e4c32024521bf0e
Lucas de Man <e@mail.com>
2024-03-11 12:44:24 +0100

    begin of setup of the network

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import torch
+import numpy as np
+from torch import nn
+from torch.autograd import Variable
+
+np.random.seed(5974)
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+class Linear_regression(nn.Module):
+    def __init__(self, windowsize=256):
+        super().__init__()
+        self.windowsize = windowsize
+        self.linear = torch.nn.Linear((2*windowsize + 1), 2)
+        self.biasTensor = torch.tensor([1], dtype=torch.float32)
+
+    def forward(self, x):
+        x = torch.cat((x, x, self.biasTensor))
+        return self.linear(x)
+
+model = Linear_regression().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+
+BATCHSIZE = 10
+epochs = 5000
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    loss = Variable(torch.tensor(0, dtype=torch.float32), requires_grad=True)
+
+    if epoch % (epochs // 10) == 0:
+        print("info")
 
 
-if __name__ == '__main__':
-    print("test")
 
--- midi/wav_combiner.py
+++ midi/wav_combiner.py
@@ -1,17 +1,25 @@
 from scipy.io.wavfile import read, write
 import numpy as np
 
+#making the music mean 0
+pianoArr = []
+violinArr = []
 for p in range(8):
-    for v in range(8):
+    piano = read("wavs/piano" + str(p) + ".wav")[1][:131072, 0]
+    piano = piano - np.mean(piano)
+    pianoArr.append(piano)
+for v in range(8):
+    violin = read("wavs/piano" + str(v) + ".wav")[1][:131072, 0]
+    violin = violin - np.mean(violin)
+    violinArr.append(violin)
+
+for p, piano in enumerate(pianoArr):
+    for v, violin in enumerate(violinArr):
         for p_vs_v in range(1, 11):
-            piano = read("wavs/piano" + str(p) + ".wav")
-            violin = read("wavs/violin" + str(v) + ".wav")
-            piano_audio = piano[1][:131072, 0]
-            violin_audio = violin[1][:131072, 0]
-            combP = piano_audio + violin_audio / 10 * p_vs_v
+            combP = piano + violin / 10 * p_vs_v
             combP = combP.astype(np.int16)
             if p_vs_v != 10:
-                combV = piano_audio / 10 * p_vs_v + violin_audio
+                combV = piano / 10 * p_vs_v + violin
                 combV = combV.astype(np.int16)
                 write("wav_comb/comb" + str(10*p_vs_v) + "-100_" + str(p) + "-" + str(v) + ".wav", rate=44100, data=combV)
             write("wav_comb/comb100-" + str(10*p_vs_v) + "_" + str(p) + "-" + str(v) + ".wav", rate=44100, data=combP)
